3ef641d03739ad04902755fe80101b721c7655df
prakash-amit <amit.prakash.se@gmail.com>
2021-07-23 21:08:04 +0530

    creating navbar using react-router links

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList'
 import ArticlePage from './pages/ArticlePage'
+import NavBar from './NavBar';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 function App() {
   return (
     <Router>
     <div className="App">
+      <NavBar />
       <div id="page-body">
       <Route path = "/" component = {HomePage} exact />
       <Route path = "/about" component = {AboutPage} exact />
