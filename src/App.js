e1e23875fd258cc09c71aa0a2bf12453b5abcb6f
prakash-amit <amit.prakash.se@gmail.com>
2021-07-23 21:01:47 +0530

    create blog pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList'
+import ArticlePage from './pages/ArticlePage'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="page-body">
+      <Route path = "/" component = {HomePage} exact />
+      <Route path = "/about" component = {AboutPage} exact />
+      <Route path = "/articles-list" component = {ArticlesList} exact />
+      <Route path = "/article" component = {ArticlePage} exact />
+      </div>
+
     </div>
+    </Router>
+
   );
 }
 
---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ArticlesList = () => (
+    <>
+    <h1>Articles List</h1>
+    </>
+)
+
+export default ArticlesList;
