82fa8e94ba691312171000516fa78dbc6ed0f627
prakash-amit <amit.prakash.se@gmail.com>
2021-07-25 17:18:55 +0530

    introduction to useState and useEffect

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ArticlesList from '../components/ArticlesList';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
@@ -6,6 +6,13 @@ import NotFoundPage from './NotFoundPage';
 const ArticlePage = ({match}) => {
     const name = match.params.name;
     const article = articles.find( article => article.name === name)
+
+    const [articleInfo, setArticleInfo] = useState({upvotes : 0, comments : []});
+
+    useEffect( () => {
+        setArticleInfo({upvotes : 3});
+    })
+
     const relatedArticles = articles.filter( article => article.name !== name);
     if( !article ) {
         return (
@@ -15,6 +22,7 @@ const ArticlePage = ({match}) => {
     return (
         <>
         <h1>{article.title}</h1>
+        <p>This article has been upvoted {articleInfo.upvotes} times.</p>
         {article.content.map(
             (paragraph,key) => (
                 <p key={key}>{paragraph}</p>
