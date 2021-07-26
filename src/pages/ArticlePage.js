5d2fb2e438c7b7cd17b34c5c302076c7423d9b7c
prakash-amit <amit.prakash.se@gmail.com>
2021-07-26 08:05:59 +0530

    displaying comments related to article on article page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
 
@@ -34,6 +35,7 @@ const ArticlePage = ({match}) => {
                 <p key={key}>{paragraph}</p>
             )
         )}
+        <CommentsList comments = {articleInfo.comments} />
         <h3>Other related articles:</h3>
         <ArticlesList articles = {relatedArticles}/>
         </>
